be9d28b7bfe58f49dc62221ea29bde48ddffd882
t_murano <viceversa.ue700@gmail.com>
2016-04-19 22:24:02 +0900

    Modify CategoryHeader daysLeft format

--- src/components/CategoryHeader.tsx
+++ src/components/CategoryHeader.tsx
@@ -2,18 +2,31 @@ import * as React from 'react'
 import Category from '../models/Category'
 
 interface CategoryHeaderProps {
-  daysLeft: string
+  daysLeft: number
   categoryName: string
   schedule: string
   image: string
 }
 
 class CategoryHeader extends React.Component<CategoryHeaderProps, void> {
+  private formatDaysLeft(daysLeft: number) {
+    switch (daysLeft) {
+      case 0:
+        return '今日'
+      case 1:
+        return '明日'
+      case 2:
+        return '明後日'
+      default:
+        return `${daysLeft}日後`
+    }
+  }
+
   public render() {
     return (
       <div class="accordion-heading">
         <a class="accordion-toggle collapsed" style="height:168.25px" data-toggle="collapse" data-parent="#accordion" href="#collapse0">
-          <div class="left-day">{this.props.daysLeft}日後</div>
+          <div class="left-day">{this.formatDaysLeft(this.props.daysLeft)}</div>
           <div class="accordion-table">
             <img src={this.props.image} alt={this.props.categoryName}/>
           </div>
